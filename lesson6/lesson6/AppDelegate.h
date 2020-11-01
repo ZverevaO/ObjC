5266ebeda301a28f70cc4c4b54f819329024527b
Oksana_Zvereva <zvereva.oksana@gmail.com>
2020-11-01 12:48:52 +0500

    lesson6 GCD

---
+++ lesson6/lesson6/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  lesson6
+//
+//  Created by Оксана Зверева on 01.11.2020.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
